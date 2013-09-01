fec002285dd85e42ca89d14cd8a5bbc05980ef4b
blahgeek <blahgeek@gmail.com>
2013-09-01 23:04:42 +0800

    user login and auth works

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -2,18 +2,31 @@ package controllers;
 
 import play.*;
 import play.mvc.*;
+import play.data.*;
 
 import views.html.*;
+import models.*;
 
 public class Application extends Controller {
   
     @Security.Authenticated(Secured.class)
     public static Result index() {
-        return ok(index.render("Your new application is ready."));
+        return ok(index.render("Your name is " + session().get("username")));
     }
 
+    static Form<User> loginForm = Form.form(User.class);
+
     public static Result login() {
-        return TODO;
+        return ok(login.render(loginForm));
+    }
+
+    public static Result auth() {
+        Form<User> form = loginForm.bindFromRequest();
+        if(form.hasErrors() || 
+                User.auth(form.get().username, form.get().password) == null)
+            return badRequest("invalid");
+        session("username", form.get().username);
+        return redirect(routes.Application.index());
     }
   
 }
